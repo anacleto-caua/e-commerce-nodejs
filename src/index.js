0d0ea1cccd4d38097ef932c20bf6e0eda7ea2101
anacleto-caua <anacleto.for.work@gmail.com>
2022-08-31 14:34:03 -0300

    Adding bootstrap

--- src/index.js
+++ src/index.js
@@ -1,5 +1,17 @@
 const express = require('express');
+const path = require('path')
 
 const app = express();
 
 app.listen(8000);
+
+app.use('/css', express.static(path.join(_dirname, 'node_modules/bootstrap/dist/css')))
+app.use('/js', express.static(path.join(_dirname, 'node_modules/bootstrap/dist/js')))
+app.use('/js', express.static(path.join(_dirname, 'node_modules/jquery/dist')))
+
+// Rotas
+
+app.get("/", (req, res) => {
+    res.sendFile(path.join(__dirname, 'views/index.html'))
+});
+  
